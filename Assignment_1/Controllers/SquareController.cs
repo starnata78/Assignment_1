a0f98f9bbb5e7ac7bac99f54c07d3a3393685f53
starnata <starnata_k@yahoo.com>
2021-02-17 16:50:51 -0500

    Comments for Q 2

---
+++ Assignment_1/Controllers/SquareController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_1.Controllers
+{
+    /* objective: crate a method which revieve an return its square.
+     * <summary>
+     * This code revieves an int and returns the square of the number.
+     * <param name -"id"> the input number</param>
+     * </summary>
+     * <example>
+     * Get api/Square/-2 -> 4
+     * </example>
+     */
+
+public class SquareController : ApiController
+    {
+        public int Get(int id)
+        {
+            return (id);
+        }
+    }
+}
