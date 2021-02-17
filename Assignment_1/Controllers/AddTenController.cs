7b19a2626c72f48dcb39738d46a385097d45057b
starnata <starnata_k@yahoo.com>
2021-02-17 16:29:43 -0500

    comments for question 1

--- Assignment_1/Controllers/AddTenController.cs
+++ Assignment_1/Controllers/AddTenController.cs
@@ -6,9 +6,21 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace Assignment_1.Controllers
+    /* objective: crate a method which revieve an int and adds a value to it.
+     * <summary>
+     * This code revieves an int and adds 10 to it.
+     * <param name -"id"> the input number</param>
+     * </summary>
+     * <example>
+     * Get api/AddTen/21 -> 31
+     * </example>
+     */
 {
     public class AddTenController : ApiController
     {
-        // I am making a change
+        public int Get(int id)
+        {
+            return id+10;
+        }
     }
 }
