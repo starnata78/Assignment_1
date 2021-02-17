28572bfad82c63d45fde4eab08154d54fb81754e
starnata <starnata_k@yahoo.com>
2021-02-17 17:42:41 -0500

    Question 3

--- Assignment_1/Controllers/AddTenController.cs
+++ Assignment_1/Controllers/AddTenController.cs
@@ -6,15 +6,16 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace Assignment_1.Controllers
-    /* objective: crate a method which revieve an int and adds a value to it.
-     * <summary>
-     * This code revieves an int and adds 10 to it.
-     * <param name -"id"> the input number</param>
-     * </summary>
-     * <example>
-     * Get api/AddTen/21 -> 31
-     * </example>
-     */
+/* objective: crate a method which revieve an int and adds a value to it.
+ * <summary>
+ * This code revieves an int and adds 10 to it.
+ * <param name -"id"> the input number</param>
+ * </summary>
+ * <example>
+ * Get api/AddTen/21 -> 31
+ * </example>
+ */
+// // GET api/AddTen
 {
     public class AddTenController : ApiController
     {
---
+++ Assignment_1/Controllers/GreetingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_1.Controllers
+{
+    /* objective: crate a method which gets an int and returns a message of concatenated string and int.
+     * <summary>
+     * This code gets an intenger and returns a message.
+     * </summary>
+     * <example>
+     * Post/api/Greeting/3 -> "Greetins to 3 people!"
+     * </example>
+     */
+    // GET api/Greeting
+
+    public class GreetingController : ApiController
+    {
+        public string Get(int id)
+        {
+            int amount = id;
+            string message = "Greetings to " + amount.ToString() + " people!";
+            return message;
+        }
+
+    }
+}
+
