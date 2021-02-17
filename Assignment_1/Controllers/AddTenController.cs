7f337cfb8a17fdf2e7735254454ed499d527d610
starnata78 <78445570+starnata78@users.noreply.github.com>
2021-02-17 16:12:09 -0500

    Update AddTenController.cs

--- Assignment_1/Controllers/AddTenController.cs
+++ Assignment_1/Controllers/AddTenController.cs
@@ -7,21 +7,5 @@ using System.Web.Http;
 
 namespace Assignment_1.Controllers
 {
-        public class AddTenController : ApiController
-    {/*objective: create a method which recieves a value and adds a certain value;
-      <summary> This code recieves a value and adds ten.
-        <param name-"id">the input number</param>
-        <returns>the number plus 10</returns>
-        </summary>
-        <example> 
-        Get api/AddTen/21 -> 31
-        </example>
-        */
-        // GET api/AddTen/
-        public int Get(int id)
-        {
-            return id+10;
-        }
-    }
-    
+       
 }
